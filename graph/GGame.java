c7a39b8db3fb6341a034074977c2759f043965da
Seres Márk Dániel <seres.dani@gmail.com>
2014-05-05 20:44:57 +0200

    GGame, Gpath

---
+++ graph/GGame.java
@@ -0,0 +1,60 @@
+package graph;
+
+/**
+ * A GGame felel a Game osztaly megjeleniteseert. Elsosorban a felso savert, vagyis a Game azon attributumainak
+ * megjeleneseert, amiket a jatekosnak celszeru latnia: mana, bejutott ellensegek szama, palya
+ * szintje stb.
+ * 
+ * @author Seres
+ *
+ */
+public class GGame extends Graphic implements IView {
+
+	/**
+	 * Az Eclipse ragaszkodott hozzá. :(
+	 */
+	private static final long serialVersionUID = 4315392664606420956L;
+
+	@Override
+	public void gNotify() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addGEnemy(GEnemy ge) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GEnemy getGEnemy(Enemy e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteGEnemy(Enemy e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void highlight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deHighlight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ graph/GPath.java
@@ -0,0 +1,59 @@
+package graph;
+
+/**
+ * A GPath felel a Path osztaly megjeleniteseert, kirajzolasaert. Minden Path-hoz tartozik egy, es mindegyikhez
+ * egy Path tartozik.
+ * 
+ * @author Seres
+ *
+ */
+public class GPath extends Graphic implements IView {
+
+	/**
+	 * Az Eclipse ragaszkodott hozzá. :(
+	 */
+	private static final long serialVersionUID = 6584228015160996554L;
+
+	@Override
+	public void gNotify() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addGEnemy(GEnemy ge) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GEnemy getGEnemy(Enemy e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteGEnemy(Enemy e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void highlight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deHighlight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
