b4147e4b2e761c8012fca91917c51581b7de5aea
Seres Márk Dániel <seres.dani@gmail.com>
2014-05-12 13:10:33 +0200

    nem mennek össze a pályák

--- graph/GCell.java
+++ graph/GCell.java
@@ -24,6 +24,7 @@ public abstract class GCell extends Graphic implements IView {
 	public GCell() {
 		this.setPreferredSize(cellSize);
 		this.setSize(cellSize);
+		this.setMinimumSize(cellSize);
 	}
 	
 	/**
